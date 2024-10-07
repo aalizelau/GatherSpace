f1019d9ffb3c42f55316f66e6c78b2b882a54928
Aalizelau <aalizelau@gmail.com>
2024-10-08 01:38:17 +0800

    add fetch post on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,9 @@
-export default function Home() {
+
+import { fetchPosts } from "@/lib/actions/thread.actions";
+
+export default async function Home() {
+  const result = await fetchPosts(1,30);
+  console.log(result)
     return(
       <main>
         <h1>Threads Home</h1>
