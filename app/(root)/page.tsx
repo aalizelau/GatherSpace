9b84fbf71522c37fd35ffe1d95a7bd0166abcc4d
Aalizelau <aalizelau@gmail.com>
2024-10-08 01:55:04 +0800

    display thread card on home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,46 @@
 
-import { fetchPosts } from "@/lib/actions/thread.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
+import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+import ThreadCard from "@/components/cards/ThreadCard";
+
+
+export default async function Home(){
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
   const result = await fetchPosts(1,30);
-  console.log(result)
-    return(
-      <main>
-        <h1>Threads Home</h1>
-      </main>
-    )
+  
+  return (
+    <>
+      <h1 className='head-text text-left'>Home</h1>
+
+      <section className='mt-9 flex flex-col gap-10'>
+        {result.posts.length === 0 ? (
+          <p className='no-result'>No threads found</p>
+        ) : (
+          <>
+            {result.posts.map((post) => (
+              <ThreadCard
+                key={post._id}
+                id={post._id}
+                currentUserId={user.id}
+                parentId={post.parentId}
+                content={post.text}
+                author={post.author}
+                community={post.community}
+                createdAt={post.createdAt}
+                comments={post.children}
+              />
+            ))}
+          </>
+        )}
+      </section>
+    </>
+  );
 }
