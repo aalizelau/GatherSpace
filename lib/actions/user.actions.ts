752236c0f6b951b1819ce576855b8fb9ee792394
Aalizelau <aalizelau@gmail.com>
2024-10-11 02:02:12 +0800

    fix profile display error

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -6,6 +6,7 @@ import { revalidatePath } from "next/cache";
 
 import User from "../models/user.model";
 import Thread from "../models/thread.model";
+import Community from "../models/community.model";
 
 import { connectToDB } from "../mongoose";
 
@@ -14,10 +15,10 @@ export async function fetchUser(userId: string) {
       connectToDB();
   
       return await User.findOne({ id: userId })
-      //.populate({
-       // path: "communities",
-       // model: Community,
-     // });
+      .populate({
+       path: "communities",
+       model: Community,
+     });
     } catch (error: any) {
       throw new Error(`Failed to fetch user: ${error.message}`);
     }
@@ -131,11 +132,11 @@ export async function fetchUserPosts(userId: string) {
       path: "threads",
       model: Thread,
       populate: [
-        // {
-        //   path: "community",
-        //   model: Community,
-        //   select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
-        // },
+        {
+          path: "community",
+          model: Community,
+          select: "name id image _id", // Select the "name" and "_id" fields from the "Community" model
+        },
         {
           path: "children",
           model: Thread,
